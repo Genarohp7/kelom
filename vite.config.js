badae657f316a85f7f711a2190b7aec514ec332d
genarohp <genarohp7@gamil.com>
2025-11-27 21:34:12 -0600

    Configurar deploy a GitHub Pages con Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // 👇 MUY IMPORTANTE para GitHub Pages (proyecto, NO user.github.io)
+  base: "/KELOM/", // cambia KELOM por el NOMBRE REAL DE TU REPO
+});
