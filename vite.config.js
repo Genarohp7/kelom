e7f44251053d5ab148c425c9c35d9fd3dfa92369
genarohp <genarohp7@gamil.com>
2025-11-27 22:05:34 -0600

    config for github

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // 👇 MUY IMPORTANTE para GitHub Pages (proyecto, NO user.github.io)
-  base: "/KELOM/", // cambia KELOM por el NOMBRE REAL DE TU REPO
+  base: "/kelom/", // 👈 nombre del repo
 });
