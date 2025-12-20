2df4e2f61c8f73c602a307b7999c7c288b45cbac
genarohp <genarohp7@gamil.com>
2025-12-19 21:54:50 -0600

    fixed on app.jsx for providers

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,12 @@ import NosotrosPage from "./pages/AboutPage.jsx";
 import BusinessAreaPage from "./pages/Business/Pages/BusinessAreaPage.jsx";
 import BusinessBenefitsPage from "./pages/Business/Pages/BusinessBenefitsPage.jsx";
 
-// 🔹 NUEVOS IMPORTS PARA LOS FORMULARIOS
+// 🔹 Formularios de empresas
 import BusinessLoginPage from "./pages/Business/Pages/BusinessLoginPage.jsx";
 import BusinessRegisterPage from "./pages/Business/Pages/BusinessRegisterPage.jsx";
+
+// 🔹 Detalle de proveedor (AJUSTA la ruta del import si tu archivo está en otra carpeta)
+import VenueDetailPage from "./pages/VenueDetailPage.jsx";
 
 function App() {
   const location = useLocation();
@@ -32,13 +35,17 @@ function App() {
 
           {/* Área de empresas (usa header/footer propios) */}
           <Route path="/empresas" element={<BusinessAreaPage />} />
-          <Route path="/empresas/beneficios" element={<BusinessBenefitsPage />} />
+          <Route
+            path="/empresas/beneficios"
+            element={<BusinessBenefitsPage />}
+          />
 
-          {/* 🔹 LOGIN PROVEEDORES */}
+          {/* Login / Registro proveedores */}
           <Route path="/empresas/acceso" element={<BusinessLoginPage />} />
-
-          {/* 🔹 REGISTRO PROVEEDORES */}
           <Route path="/empresas/registro" element={<BusinessRegisterPage />} />
+
+          {/* 🔹 Detalle de proveedor */}
+          <Route path="/proveedores/:id" element={<VenueDetailPage />} />
 
           {/* Cualquier ruta rara manda al Home */}
           <Route path="*" element={<HomePage />} />
